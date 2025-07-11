12658f3511ca7bad64616cdb02832d965ab95cc8
Mohamed Montasser <mmontasser43@gmail.com>
2025-07-11 05:57:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from together import Together
 TOGETHER_API_KEY = "08e51872d0f3ce01af96b278da8f1f7757e485f71237501ecf42e8cab4661bb8"  # Replace this with your real key
 
 # Initialize Together.ai client
-client = Together(api_key=TOGETHER_API_KEY)
-
+client = Together(api_key=st.secrets["TOGETHER_API_KEY"])
 # Set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float32
